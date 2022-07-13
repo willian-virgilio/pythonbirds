4b1f5aa81ff470d646552fb7c84f2c2fc615cca7
willian-virgilio <willian.virgilio@gmail.com>
2022-07-13 20:08:49 -0300

    colisão padrão com obstaculos e com o chão

--- fase.py
+++ fase.py
@@ -91,6 +91,7 @@ class Fase():
         :param angulo: ângulo de lançamento
         :param tempo: Tempo de lançamento
         """
+
         for passaro in self._passaros:
             if not passaro.foi_lancado():
                 passaro.lancar(angulo,tempo)
@@ -106,6 +107,11 @@ class Fase():
         :param tempo: tempo para o qual devem ser calculados os pontos
         :return: objeto do tipo Ponto
         """
+        for passaro in self._passaros:
+            passaro.calcular_posicao(tempo)
+            for alvo in self._obstaculos +self._porcos:
+                passaro.colidir(alvo,self.intervalo_de_colisao)
+            passaro.colidir_com_chao()
         pontos=[self._transformar_em_ponto(a) for a in self._passaros+self._obstaculos+self._porcos]
 
         return pontos
